3ee15f3e465e8558787b6d27ec6a23798d77e95a
kavos113 <kohei.hadano@gmail.com>
2024-10-01 10:53:57 +0900

    detailview

---
+++ src/renderer/src/components/list/CourseDetailView.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import { CourseDetail } from '../../../../types/course.js'
+
+const props = defineProps<{
+  data: CourseDetail
+}>()
+</script>
+
+<template>
+  <div>
+    <div class="wrapper">
+      <div class="title">
+        <p class="jaTitle">{{ props.data.title.slice(7) }}</p>
+        <p class="enTitle">{{ props.data.english_title }}</p>
+      </div>
+      <div class="data">
+        <dl class="dataItem">
+          <dt>開講元</dt>
+          <dd>{{ props.data.department }}</dd>
+        </dl>
+        <dl class="dataItem">
+          <dt>担当教員</dt>
+          <dd>
+            <a :href="props.data.lecturer[0].url">{{ props.data.lecturer[0].name }}</a>
+          </dd>
+        </dl>
+        <dl class="dataItem">
+          <dt>授業形態</dt>
+          <dd>{{ props.data.lecture_type }}</dd>
+        </dl>
+        <dl class="dataItem">
+          <dt>曜日・時限(講義室)</dt>
+          <dd>
+            <span v-for="timetable in props.data.timetable" :key="timetable.day_of_week">
+              {{ timetable.day_of_week }}{{ timetable.period }}限({{ timetable.room }})
+            </span>
+          </dd>
+        </dl>
+        <dl class="dataItem">
+          <dt>開講学期</dt>
+          <dd>{{ props.data.start }}</dd>
+        </dl>
+        <div class="dataItem">
+          <dl class="dataItemChild">
+            <dt>科目コード</dt>
+            <dd>{{ props.data.code }}</dd>
+          </dl>
+          <dl class="dataItemChild">
+            <dt>単位数</dt>
+            <dd>{{ props.data.credits }}</dd>
+          </dl>
+        </div>
+        <div class="dataItem">
+          <dl class="dataItemChild">
+            <dt>シラバス更新日</dt>
+            <dd>{{ props.data.sylbs_update }}</dd>
+          </dl>
+          <dl class="dataItemChild">
+            <dt>言語</dt>
+            <dd>{{ props.data.language }}</dd>
+          </dl>
+        </div>
+      </div>
+      <div class="details" v-html="props.data.detailsHTML"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  padding: 1rem;
+  font-size: 0.8rem;
+}
+
+.title {
+  margin-bottom: 1rem;
+  border-bottom: 2px solid #42d392;
+  padding: 1rem 0 0.5rem;
+}
+
+.jaTitle {
+  font-size: 2rem;
+  padding-top: 1rem;
+}
+
+.data {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+}
+
+.dataItem {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0.2rem;
+  width: 100%;
+}
+
+.dataItem dt {
+  width: 140px;
+  font-weight: bold;
+}
+
+.dataItemChild {
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+}
+</style>
--- src/renderer/src/components/list/CourseListTable.vue
+++ src/renderer/src/components/list/CourseListTable.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import { CourseListItem } from '../../../../types/course.js'
+import { CourseListItem, CourseDetail } from '../../../../types/course.js'
 import { ref } from 'vue'
 import CourseListCard from '@renderer/components/list/CourseListCard.vue'
+import CourseDetailView from '@renderer/components/list/CourseDetailView.vue'
 
 const props = defineProps<{
   data: CourseListItem[]
 }>()
 
 const isDetail = ref<boolean>(false)
+const isShowCourseDetail = ref<boolean>(true)
 
 const timetable = ref<string[]>([])
 props.data.forEach((item) => {
@@ -18,6 +20,42 @@ props.data.forEach((item) => {
     .join('\n')
   timetable.value.push(text)
 })
+
+const openDetail = () => {
+  isDetail.value = !isDetail.value
+}
+
+const sample: CourseDetail = {
+  code: 'PHY.S209',
+  title: '2024年度　熱力学（物理）(講義）',
+  english_title: 'Thermodynamics (Physics) (Lecture)',
+  lecturer: [
+    {
+      name: '笹本 智弘',
+      url: 'https://www.ocw.titech.ac.jp/index.php?module=General&action=StaffInfo&id=489'
+    }
+  ],
+  department: '物理学系',
+  start: '2024年度 3Q',
+  sylbs_update: '2024年3月29日',
+  lecture_type: '講義 (対面型)',
+  language: '日本語',
+  detailsHTML:
+    '<div id="overview">\n\n          <div class="cont-sec">\n            <h3>講義の概要とねらい</h3>\n            <p>熱力学は物質の状態や状態変化を理解するための基礎的学問である。本講義では，主に気体を対象として，熱力学の法則，エントロピー，自由エネルギー，平衡条件などの熱力学の基礎を講義する。<br>受講生は熱力学の法則やエントロピー，自由エネルギーなどの概念と，その数学的記述を修得し，これを基に熱力学現象を理解し，未知の熱力学問題を解明する能力を養う。</p>\n          </div>\n\n          <div class="cont-sec">\n            <h3>到達目標</h3>\n            <p>本講義を履修することにより，以下の知識と能力を修得する。<br>1) 熱力学の法則を理解できる。<br>2) エントロピー，自由エネルギー，化学ポテンシャルなどの概念を理解できる。<br>3) 熱力学の数学的記述法を修得し，式が意味する物理を説明できる。</p>\n          </div>\n\n\n          <div class="cont-sec"><h3>キーワード</h3><p style="white-space: pre-line;">内部エネルギー，熱力学第一法則，準静的過程，定積変化，定圧変化，等温変化，断熱変化，カルノーサイクル，熱力学第二法則，効率，熱力学温度，エントロピー，可逆と不可逆過程，平衡条件，自由エネルギー，マックスウェルの関係式，相平衡，化学ポテンシャル，空洞放射，ゴム弾性，磁性体</p></div>\n\n          <div class="cont-sec">\n            <h3>学生が身につける力(ディグリー・ポリシー)</h3>\n            <table id="learing_skill2" class="overview">\n              <tbody>\n              <tr>\n                <td class="skill_checked2" style="border-top-width: 0px">✔ <span style="color: #000">専門力</span></td>\n                <td class="skill_checked3" style="border-top-width: 0px"><span>教養力</span></td>\n                <td class="skill_checked3" style="border-top-width: 0px"><span>コミュニケーション力</span></td>\n                <td class="skill_checked3" style="border-top-width: 0px"><span>展開力(探究力又は設定力)</span></td>\n                <td class="skill_checked2" style="border-top-width: 0px">✔ <span style="color: #000">展開力(実践力又は解決力)</span></td>\n              </tr>\n              </tbody>\n            </table>\n          </div>\n\n          <div class="cont-sec">\n            <h3>授業の進め方</h3>\n            <p>各講義の3/4は基礎的内容について，残る1/4は発展的応用的内容についての解説に充てる。講義内容の確実な理解と応用力を養うために，講義内容に関連した演習問題を演習で出題する。</p>\n          </div>\n\n          <div class="cont-sec">\n            <h3>授業計画・課題</h3>\n            <table id="lecture_plans" class="overview">\n              <thead>\n              <tr>\n                <th>&nbsp;</th>\n                <th>授業計画</th>\n                <th>課題</th>\n              </tr>\n              </thead>\n              <tbody>\n              <tr>\n                <td class="number_of_times">第1回</td>\n                <td class="plan">熱力学第一法則とカルノーサイクル</td>\n                <td class="assignment">熱力学第一法則を説明せよ。カルノーサイクルを説明せよ。</td>\n              </tr>\n              <tr>\n                <td class="number_of_times">第2回</td>\n                <td class="plan">熱力学第二法則</td>\n                <td class="assignment">熱力学第二法則を説明せよ。</td>\n              </tr>\n              <tr>\n                <td class="number_of_times">第3回</td>\n                <td class="plan">エントロピー</td>\n                <td class="assignment">エントロピーを説明せよ。</td>\n              </tr>\n              <tr>\n                <td class="number_of_times">第4回</td>\n                <td class="plan">平衡条件と自由エネルギー</td>\n                <td class="assignment">自由エネルギーを用いて平衡条件を説明せよ。</td>\n              </tr>\n              <tr>\n                <td class="number_of_times">第5回</td>\n                <td class="plan">熱力学諸関数</td>\n                <td class="assignment">マックウェルの関係式を導け。</td>\n              </tr>\n              <tr>\n                <td class="number_of_times">第6回</td>\n                <td class="plan">相平衡と化学ポテンシャル</td>\n                <td class="assignment">相平衡を化学ポテンシャルを用いて説明せよ。</td>\n              </tr>\n              <tr>\n                <td class="number_of_times">第7回</td>\n                <td class="plan">熱力学の応用</td>\n                <td class="assignment">ゴム弾性と磁性体の性質を熱力学的に説明せよ。</td>\n              </tr>\n              </tbody>\n            </table>\n          </div>\n\n          <div class="cont-sec">\n            <h3>授業時間外学修（予習・復習等）</h3>\n            <p>学修効果を上げるため，教科書や配布資料等の該当箇所を参照し，「毎授業」授業内容に関する予習と復習（課題含む）をそれぞれ概ね100分を目安に行うこと。</p>\n          </div>\n\n          <div class="cont-sec">\n            <h3>教科書</h3>\n            <p>特になし</p>\n          </div>\n\n          <div class="cont-sec">\n            <h3>参考書、講義資料等</h3>\n            <p>フェルミ"熱力学" 三省堂<br>田崎晴明 "熱力学" 培風館<br>など。講義中にも紹介する。</p>\n          </div>\n\n          <div class="cont-sec">\n            <h3>成績評価の基準及び方法</h3>\n            <p>到達度をレポート課題、期末試験により評価する。</p>\n          </div>\n\n          <div class="cont-sec">\n            <h3>関連する科目</h3>\n            <ul>\n              <li>PHY.S301 ： 統計力学(講義）</li>\n              <li>PHY.S312 ： 統計力学II(講義）</li>\n            </ul>\n          </div>\n\n          <div class="cont-sec">\n            <h3>履修の条件(知識・技能・履修済科目等)</h3>\n            <p>履修の条件は設けない。</p>\n          </div>\n\n\n\n        </div>',
+  credits: 1,
+  timetable: [
+    {
+      day_of_week: '火',
+      period: 3,
+      room: 'M-178(H1101)'
+    },
+    {
+      day_of_week: '火',
+      period: 4,
+      room: 'M-178(H1101)'
+    }
+  ]
+}
 </script>
 
 <template>
@@ -54,6 +92,7 @@ props.data.forEach((item) => {
       </tbody>
     </table>
   </div>
+  <CourseDetailView v-if="isShowCourseDetail" :data="sample" class="detail" />
 </template>
 
 <style scoped>
@@ -73,5 +112,15 @@ props.data.forEach((item) => {
 .table td {
   font-size: 12px;
   padding: 2px 8px;
+}
+
+.detail {
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: white;
+  width: 50%;
+  height: 100%;
+  overflow-y: scroll;
 }
 </style>
